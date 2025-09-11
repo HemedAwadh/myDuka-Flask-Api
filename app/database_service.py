a620e2d435345f20916a95afb348c05fc2e88ba8
Hemed Awadh Said <hemedawadh03@gmail.com>
2025-09-11 23:22:13 +0300

    added sqlite

--- app/database_service.py
+++ app/database_service.py
@@ -1,13 +1,22 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from datetime import datetime
+import os
 
 
 # initialize the app
 app = Flask(__name__)
 
 #set up database conn
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:admin@localhost:5432/flask_api'
+# app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:admin@localhost:5432/flask_api'
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+print("basedir-----",basedir)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database', 'flask_api.db')
+
+
+
 
 # disable events tracks object changes
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
@@ -53,6 +62,13 @@ class Payment(db.Model):
     amount = db.Column(db.Float, nullable=True)
     trans_code = db.Column(db.String(100), nullable=True)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+    #docker run -v d:\FLASK_API\app:/app/database/database.db -p 127.0.0.1:5000:80 -t flask-api
+
+    #docker run -v /home/volume-database/flask-api/app:/app/database/database.db -p 5000:80 -t flask-api
+
+    #docker run -v /home/volume-database/flask-api/app:/app/database/database.db -p 5000:80 -d -t flask-api
     
    
 
